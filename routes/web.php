777cccf32455b3bb313c2cc855b40501bfef448c
Sakanian <sakanyan2004@gmail.com>
2024-03-04 11:02:51 +0300

    DZ 3

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Руты для работы с тестовым контроллером
 use App\Http\Controllers\MainController;
 Route::get('/', [MainController::class, 'show_all_articles']);
 Route::get('/one_article', [MainController::class, 'show_one_article']);
 Route::get('/about_us', [MainController::class, 'show_about_us']);
 Route::get('/contacts', [MainController::class, 'show_contacts']);
 
+// Руты для работы с пользователями
 use App\Http\Controllers\AuthController;
 Route::get('/create_user', [AuthController::class, 'create']);
 Route::post('/register', [AuthController::class, 'register']);
 
-
-use App\Http\Controllers\ArticleController;
 // Руты для работы со статьями
+use App\Http\Controllers\ArticleController;
 Route::resource('/article', ArticleController::class);
+
+// Руты для работы с комментариями к статьям
+use App\Http\Controllers\CommentController;
+Route::group(['prefix' => '/comment'], function() {
+	Route::post('/store', [CommentController::class, 'store']);
+	Route::get('/edit/{id}', [CommentController::class, 'edit']);
+	Route::post('/update/{id}', [CommentController::class, 'update']);
+	Route::get('/delete/{id}', [CommentController::class, 'delete']);
+});
