04208d5b8473e1565cd5c83bed74163f063a392a
Sakanian <sakanyan2004@gmail.com>
2024-03-17 16:15:29 +0300

    Добавление планировщика задач

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Руты для работы с тестовым контроллером
-use App\Http\Controllers\MainController;
+// Общие руты для MainController
 Route::get('/', [MainController::class, 'show_all_articles']);
 Route::get('/one_article', [MainController::class, 'show_one_article']);
 Route::get('/about_us', [MainController::class, 'show_about_us']);
 Route::get('/contacts', [MainController::class, 'show_contacts']);
 
 // Руты для работы с пользователями
-use App\Http\Controllers\AuthController;
 Route::get('/register', [AuthController::class, 'registration']);
 Route::post('/create_user', [AuthController::class, 'create_user']);
 Route::get('/login', [AuthController::class, 'login'])->name('login');
@@ -29,12 +31,11 @@ Route::post('/authenticate', [AuthController::class, 'authenticate']);
 Route::get('/logout', [AuthController::class, 'logOut']);
 
 // Руты для работы со статьями
-use App\Http\Controllers\ArticleController;
 Route::resource('/article', ArticleController::class);
+Route::get('article/{article}', [ArticleController::class, 'show'])->name('article.show')->middleware('path-counter');
 
 // Руты для работы с комментариями к статьям
-use App\Http\Controllers\CommentController;
-Route::group(['prefix' => '/comment'], function() {
+Route::group(['prefix' => '/comment', 'middleware' => 'auth:sanctum'], function() {
 	Route::get('/', [CommentController::class, 'index'])->name('comments');
 	Route::post('/store', [CommentController::class, 'store']);
 	Route::get('/edit/{id}', [CommentController::class, 'edit']);
